864cbe0bf08d865ed7bf470edb944a7d0b0195e2
waniek <gryzman@gmail.com>
2011-01-09 00:43:15 +0000

    s/wykop/wrzuta/g

---
+++ wrzutaGet/src/goBitches.java
@@ -0,0 +1,88 @@
+/**
+ * User: gj
+ * Date: 08/01/2011
+ * Time: 20:38
+ *
+ *
+ *   Public Domain
+ *
+ */
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.SecureRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class goBitches
+{
+  private SecureRandom random;
+
+  public goBitches()
+  {
+    try
+    {
+      random = SecureRandom.getInstance("SHA1PRNG");
+    }
+    catch (Exception e)
+    {
+    }
+
+    if (null == random)
+    {
+      random = new SecureRandom();
+      random.setSeed(System.currentTimeMillis() / 666);
+    }
+  }
+
+  public String getTheBitch(String inurl, String agent)
+  {
+    String bitchesUrl;
+    try
+    {
+      String filename = inurl.split("/")[4];
+      String victim = inurl.split("/")[2].replace(".wrzuta.pl", "");
+
+      String requestUrl = "http://" + victim + ".wrzuta.pl/xml/plik/" + filename + "/wrzuta.pl/undefined/" + Math.abs(random.nextInt());
+
+      URL url = new URL(requestUrl);
+      URLConnection urlc = url.openConnection();
+
+      urlc.setRequestProperty("User-Agent", agent);
+      BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(urlc.getInputStream()));
+
+      String line;
+      String lines = "";
+
+      // read all bloody lines
+      while ((line = bufferedreader.readLine()) != null)
+      {
+        lines += line;
+      }
+
+      Matcher matcher = Pattern.compile("<fileId>(.*)</fileId>").matcher(lines);
+
+      String slave = "";
+
+      while (matcher.find())
+      {
+        slave = matcher.group().substring(17, matcher.group().length() - 12);
+      }
+
+      bitchesUrl = slave;
+    }
+    catch (Exception exception)
+    {
+      System.out.println((new StringBuilder()).append("Exception e = ").append(exception).toString());
+      exception.printStackTrace();
+      bitchesUrl = null;
+    }
+
+    return bitchesUrl;
+  }
+
+}
---
+++ wrzutaGet/src/wrzutaGet.java
@@ -0,0 +1,98 @@
+/**
+ * User: gj
+ * Date: 08/01/2011
+ * Time: 20:10
+ *
+ *   Public Domain
+ *
+ */
+
+import java.io.*;
+
+
+public class wrzutaGet
+{
+  public static void main(String[] args)
+  {
+
+    if (0 == args.length || 2 < args.length)
+    {
+      System.out.println("give us a file with wrzuta URLs and another name if you want it to be saved there!");
+      System.out.println("otherwise output URLs will go to the stdout");
+      return;
+    }
+
+    /*
+       try to open the bloody file
+     */
+    FileInputStream infile;
+    DataInputStream in;
+    BufferedReader br;
+
+    try
+    {
+      infile = new FileInputStream(args[0]);
+      in = new DataInputStream(infile);
+      br = new BufferedReader(new InputStreamReader(in));
+
+    }
+    catch (Exception e)
+    {
+      System.out.println("failed to open the source file \"" + args[0] + "\" bitch");
+      System.out.print("Reason: " + e.toString());
+      return;
+    }
+
+    /* trying to open the save file, if requested */
+    FileWriter outfile = null;
+    try
+    {
+      if (2 == args.length)
+      { outfile = new FileWriter(args[1], false); }
+    }
+    catch (Exception e)
+    {
+      System.out.println("failed to open the destination file \"" + args[1] + "\" bitch");
+      System.out.print("Reason: " + e.toString());
+      return;
+    }
+
+    try
+    {
+      String strLine;
+      goBitches bitches = new goBitches();
+
+      while ((strLine = br.readLine()) != null)
+      {
+        strLine = strLine.trim();
+        if (!"".equals(strLine))
+        {
+          String dl;
+          dl = bitches.getTheBitch(strLine, "bitch");
+          if (null != outfile)
+          {
+            outfile.write(dl);
+            outfile.write(System.getProperty("line.separator"));
+          }
+          else
+          {
+            System.out.println(dl);
+          }
+        }
+      }
+      in.close();
+      if (null != outfile)
+      {
+        outfile.close();
+      }
+    }
+    catch (Exception e)
+    {
+      System.out.println("the job has failed, bitch");
+      System.out.print("Reason: " + e.toString());
+      return;
+    }
+
+    System.out.println("done, bitches!");
+  }
+}
